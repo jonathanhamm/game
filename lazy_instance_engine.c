3fe3379c14431069844d364007f3f018aea50006
Jonathan Hamm <jonathan-hamm@utulsa.edu>
2021-09-26 18:12:02 -0500

    LazyInstanceParser: got lexing for lazy instances working

--- lazy_instance_engine.c
+++ lazy_instance_engine.c
@@ -1,16 +1,15 @@
 #include "lazy_instance_engine.h"
-
-#include "all.h"
-
+#include "log.h"
 #include <ctype.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 #define LZTOK_LEX_LEN 64
 
 typedef enum lztok_type_e lztok_type_e;
 
 typedef struct lztok_s lztok_s;
+typedef struct lztok_list_s lztok_list_s;
 
 enum lztok_type_e {
 	LZTYPE_NUM,
@@ -26,15 +25,26 @@ struct lztok_s {
 	lztok_s *next;
 };
 
-static lztok_s *lz_add_tok(lztok_s **curr, char *lexeme);
+struct lztok_list_s {
+  lztok_s *head;
+  lztok_s *tail;
+};
+
+static void lz_add_tok(lztok_list_s *list, char *lexeme, lztok_type_e type);
+static lztok_s *lex(char *src);
+static char *dupstr(char *str);
 
 double lazy_epxression_compute(char *src, double val) {
+  char *nsrc = dupstr(src);
+  lztok_s *toklist = lex(nsrc);
 	return 0;
 }
 
 lztok_s *lex(char *src) {
 	char bck;
-	char *fptr = src, *bptr = src;
+	char *fptr = src, *bptr;
+
+  lztok_list_s toklist = {NULL, NULL};
 
 	while (*fptr) {
 		switch (*fptr) {
@@ -46,15 +56,19 @@ lztok_s *lex(char *src) {
 				fptr++;
 				break;
 			case '+':
+        lz_add_tok(&toklist, "+", LZTYPE_ADDOP);
 				fptr++;
 				break;
 			case '-':
+        lz_add_tok(&toklist, "-", LZTYPE_ADDOP);
 				fptr++;
 				break;
 			case '*':
+        lz_add_tok(&toklist, "*", LZTYPE_MULOP);
 				fptr++;
 				break;
 			case '/':
+        lz_add_tok(&toklist, "/", LZTYPE_MULOP);
 				fptr++;
 				break;
 			default:
@@ -63,25 +77,59 @@ lztok_s *lex(char *src) {
 					while (isdigit(*++fptr));
 					bck = *fptr;
 					*fptr = '\0';
+          lz_add_tok(&toklist, bptr, LZTYPE_NUM);
+          *fptr = bck;
 				} 
 				else if (isalpha(*fptr)) {
+          bptr = fptr;
+          while (isalpha(*++fptr));
+          bck = *fptr;
+          *fptr = '\0';
+          lz_add_tok(&toklist, bptr, LZTYPE_NUM);
+          *fptr = bck;
 				}
 				else {
+          log_error("lexical error: unknown character: %c", *fptr);
+          fptr++;
 				}
 				break;
 		}
 	}
 
+  lztok_s *t = toklist.head;
+  while (t) {
+    log_info("parsed token: %s - %d", t->lexeme, t->type);
+    t = t->next;
+  }
 }
 
-void lz_add_tok(lztok_s **curr, char *lexeme) {
+void lz_add_tok(lztok_list_s *list, char *lexeme, lztok_type_e type) {
 	lztok_s *ntok = malloc(sizeof *ntok);
 	if (!ntok) {
 		log_error("failed to allocate memory for new lz token");
 		//TODO: trap
 	}
-	ntok->
+  ntok->type = type;
+  ntok->next = NULL;
+	strcpy(ntok->lexeme, lexeme);
+  if (!list->head) {
+    list->head = ntok;
+  } else {
+    list->tail->next = ntok;
+  }
+  list->tail = ntok;
+}
 
+char *dupstr(char *str) {
+  size_t len = strlen(str);
+  char *nstr = malloc(len);
+  if (!nstr) {
+    log_error("memory allocation error during string duplication");
+    //trap
+    return NULL;
+  }
+  strcpy(nstr, str);
+  return nstr;
 }
 
 
--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include "log.h"
 #include "game.h"
+#include "lazy_instance_engine.h"
+
 #include <stdio.h>
 
 int main(void) {
 	log_init(stdout);
-	bob_start();
+	//bob_start();
+  lazy_epxression_compute("a+b", 1.0);
 	log_end();
 	return 0;
 }
